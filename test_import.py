d183a4363aa5afe3a23e5256bc17e4135487282d
Ivan Pevtsov <pevtsovi@cqg.com>
2021-06-09 13:32:26 +0300

    added errorneous file

--- test_import.py
+++ test_import.py
@@ -13,15 +13,36 @@ error_file_path = './test_output/error_output.txt'
 test_output_folder = './test_output'
 
 expected_output = {
-    './test_data/valid/bank1.csv': 'date,transaction_type,amount,from,to\n'
-                             '2019-10-01,remove,99.20,198,182\n'
-                             '2019-10-02,add,2000.10,188,198\n',
-    './test_data/valid/bank2.csv': 'date,transaction_type,amount,from,to\n'
-                             '2019-03-10,remove,99.40,198,182\n'
-                             '2019-04-10,add,2123.50,188,198\n',
-    './test_data/valid/bank3.csv': 'date,transaction_type,amount,from,to\n'
-                             '2019-10-05,remove,5.07,198,182\n'
-                             '2019-10-06,add,1060.08,188,198\n',
+    './test_data/valid/bank1.csv':
+            {
+                'output': 'date,transaction_type,amount,from,to\n'
+                          '2019-10-01,remove,99.20,198,182\n'
+                          '2019-10-02,add,2000.10,188,198\n',
+
+                'error_output': ''
+            },
+
+    './test_data/valid/bank2.csv':
+            {
+                'output': 'date,transaction_type,amount,from,to\n'
+                          '2019-03-10,remove,99.40,198,182\n'
+                          '2019-04-10,add,2123.50,188,198\n',
+                'error_output': ''},
+
+    './test_data/valid/bank3.csv':
+            {
+                'output': 'date,transaction_type,amount,from,to\n'
+                          '2019-10-05,remove,5.07,198,182\n'
+                          '2019-10-06,add,1060.08,188,198\n',
+                'error_output': ''},
+
+    # this file contains a  row with broken date field so we except it to be filtered out to
+    # error output
+    './test_data/invalid/bank1.csv':
+            {
+               'output': 'date,transaction_type,amount,from,to\n'
+                         '2019-10-02,add,2000.10,188,198\n',
+               'error_output': 'Unknown string format: asdadad'}
 }
 
 
@@ -50,14 +71,22 @@ def setup_output_files():
 @pytest.mark.parametrize('expected_output_item', [*expected_output.items()])
 def test_valid_import(setup_output_files, expected_output_item):
     '''Basic use case just to check the parser works as expected'''
+
     output_file = setup_output_files[0]
-    parse_csv_file(expected_output_item[0], setup_output_files[1], setup_output_files[2])
-    expected_data = expected_output_item[1]
+    error_file = setup_output_files[2]
+    parse_csv_file(
+        expected_output_item[0],
+        setup_output_files[1],
+        setup_output_files[2]
+    )
+
+    excepted_error_data = expected_output_item[1]['error_output']
+    expected_data = expected_output_item[1]['output']
     actual_data = _get_output_data(output_file)
+    actual_error_data = _get_output_data(error_file)
 
     assert actual_data == expected_data
-
-
+    assert excepted_error_data in actual_error_data
 
 
 ################################################### Negative Tests #####################################################
@@ -70,11 +99,6 @@ def test_no_input_files(mocker):
         main.main()
         assert pytest_wrapped_e.type == SystemExit
         assert pytest_wrapped_e.value.code == 0
-
-
-
-
-
 
 
 
